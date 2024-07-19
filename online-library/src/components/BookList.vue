d7961b09494ffd16f01d96b305575b3c5aac8d9e
Dilan Bandara Ranasinghe <1128dilan@gmail.com>
2024-07-19 21:49:18 +0530

    Laravel project

---
+++ online-library/src/components/BookList.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="book in books" :key="book.id">
+        <h3>{{ book.title }}</h3>
+        <p>{{ book.description }}</p>
+        <p><strong>Genre:</strong> {{ book.genre }}</p>
+        <p><strong>Price:</strong> ${{ book.price }}</p>
+        <button @click="$emit('borrow', book.id)">Borrow</button>
+        <button @click="$emit('return', book.id)">Return</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['books']
+};
+</script>
+
+<style>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  border: 1px solid #ccc;
+  padding: 20px;
+  margin: 10px 0;
+}
+
+button {
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+  margin: 5px;
+}
+</style>
---
+++ online-library/src/components/SearchBooks.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <input v-model="searchTerm" placeholder="Search for books..." />
+    <button @click="search">Search</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      searchTerm: ''
+    };
+  },
+  methods: {
+    search() {
+      this.$emit('search', this.searchTerm);
+    }
+  }
+};
+</script>
+
+<style>
+input {
+  padding: 10px;
+  font-size: 16px;
+}
+
+button {
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+}
+</style>
