26c45bd9256bc9b6841f355a74cb499161af39db
Dilan Bandara Ranasinghe <1128dilan@gmail.com>
2024-07-20 13:24:33 +0530

    updates

--- online-library-backend/routes/api.php
+++
@@ -1,6 +0,0 @@
-use App\Http\Controllers\BookController;
-
-Route::get('/books', [BookController::class, 'index']);
-Route::post('/books', [BookController::class, 'store']);
-Route::put('/books/{id}', [BookController::class, 'update']);
-Route::delete('/books/{id}', [BookController::class, 'destroy']);
