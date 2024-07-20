235a5d19dda1793391ee815edeb94bb84c5930ab
Dilan Bandara Ranasinghe <1128dilan@gmail.com>
2024-07-20 10:28:18 +0530

    start project

---
+++ online-library-backend/routes/api.php
@@ -0,0 +1,6 @@
+use App\Http\Controllers\BookController;
+
+Route::get('/books', [BookController::class, 'index']);
+Route::post('/books', [BookController::class, 'store']);
+Route::put('/books/{id}', [BookController::class, 'update']);
+Route::delete('/books/{id}', [BookController::class, 'destroy']);
